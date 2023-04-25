85e0edd34d8c8b250c86e767ed4f0759830fa469
Konrad <konradh1997@gmail.com>
2023-04-25 16:19:24 +0200

    deleting unused files, adding readme

--- test.js
+++
@@ -1,5 +0,0 @@
-const scriptPath = "blablabla1";
-const randomNum = Math.floor(Math.random() * 1000000) + 1;
-console.log(randomNum);
-const updatedPath = scriptPath.slice(0, -1) + randomNum;
-console.log(updatedPath);
